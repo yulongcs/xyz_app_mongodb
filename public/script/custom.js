03a47d275ad18b35fc252e5044a9550765cafe98
yulongcs <yulongcs@qq.com>
2016-01-08 09:07:58 +0800

    add js

---
+++ public/script/custom.js
@@ -0,0 +1,17 @@
+﻿jQuery(document).ready(function() {
+
+/***************************************************
+// Select control
+***************************************************/
+$(function(){
+	var bindEvent = function() {
+		$("select").on("change", function(event) {
+			var obj = $(this).get(0)
+			$(this).parent(".cell-input").find(".select-value").html(obj.options[obj.selectedIndex].text)
+		});
+	}
+	bindEvent();
+});
+//Select control END
+
+});
